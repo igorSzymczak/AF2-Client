f5f16fd8950be12d2bdf9d207817079f60ffbf7d
igorSzym <igorszkropkakom@gmail.com>
2026-02-18 08:39:04 +0100

    made PvP Button not trigger shooting

--- scenes/ui/pvp_button_container.gd
+++ scenes/ui/pvp_button_container.gd
@@ -3,3 +3,9 @@ extends BetterButton
 func _on_toggled(toggled_on: bool) -> void:
 	g.request_toggle_pvp(toggled_on)
 	g.me.pvp = toggled_on
+
+func _on_mouse_entered() -> void:
+	g.can_perform_actions = false
+
+func _on_mouse_exited() -> void:
+	g.can_perform_actions = true

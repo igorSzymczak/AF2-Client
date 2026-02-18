24ec7598147f479856ab6efac1a24de10e10ac84
igorSzym <igorszkropkakom@gmail.com>
2026-02-18 13:46:44 +0100

    minor fix

--- scenes/objects/Structures/Recycle Station/recycle_station.gd
+++ scenes/objects/Structures/Recycle Station/recycle_station.gd
@@ -2,9 +2,6 @@ class_name RecycleStation extends Structure
 
 @export var progress_bar: ProgressBar
 
-func _ready() -> void:
-	progress_bar.value = 0.0
-
 func animate_progress_bar(value: float, seconds: float) -> void:
 	var tween: Tween = create_tween()
 	tween.tween_property(progress_bar, "value", value, seconds)
